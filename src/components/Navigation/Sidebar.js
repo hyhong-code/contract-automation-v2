d415d9dd2de6e82949bd163800dd73420e71b957
Haiyang Hong <hong961127@gmail.com>
2020-09-01 23:30:23 -0700

    fix Sidebar icon

--- src/components/Navigation/Sidebar.js
+++ src/components/Navigation/Sidebar.js
@@ -6,12 +6,12 @@ const Sidebar = () => {
   return (
     <nav className="sidebar">
       <div className="sidebar__icons">
-        <div className="sidebar__icons-box">
+        <a href="#!" className="sidebar__icons-box">
           <i class="sidebar__icons-icon far fa-sticky-note"></i>
-        </div>
-        <div className="sidebar__icons-box">
+        </a>
+        <a href="#!" className="sidebar__icons-box">
           <i class="sidebar__icons-icon far fa-user-circle"></i>
-        </div>
+        </a>
       </div>
     </nav>
   );

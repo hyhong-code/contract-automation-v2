d96a8101e526b5565e44e0b161308be7c3d73718
Haiyang Hong <hong961127@gmail.com>
2020-09-05 20:48:19 -0700

    Add login sign up page

--- contract/src/App.js
+++ contract/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Header from "./components/UI/Header/Header";
-import MainPageNavbar from "./components/UI/MainPageNavbar/MainPageNavbar";
+import Navbar from "./components/UI/MainPageNavbar/Navbar";
 import Footer from "./components/UI/Footer/Footer";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { StateMachineProvider, createStore } from "little-state-machine";
 import { DevTool } from "little-state-machine-devtools";
 import useHeaderToggle from "./hooks/useHeaderToggle";
 import useRedirect from "./hooks/useRedirect";
+import useLoginSignup from "./hooks/useLoginSignup";
 
 import LandingPage from "./components/Pages/LandingPage/LandingPage";
 import GetStarted from "./components/Pages/GetStarted/GetStarted";
@@ -21,6 +22,8 @@ import DownloadTO from "./components/Pages/Form/DownloadTo/DownloadTo";
 import PDF from "./components/Pages/Form/PDF/PDF";
 import Complete from "./components/Pages/Form/Complete/Complete";
 import path from "./utils/path";
+import Login from "./components/Pages/LoginSignup/Login";
+import Signup from "./components/Pages/LoginSignup/Signup";
 
 import Sidebar from "./components/Navigation/Sidebar";
 import MainPage from "./components/Pages/MainPage/MainPage";
@@ -89,13 +92,14 @@ createStore({
 function App() {
   // Hook for toggling between 2 Navbars depends on what page user is on
   const isMainPageHeader = useHeaderToggle();
+  const isLoginSignup = useLoginSignup();
   useRedirect();
 
   return (
     <StateMachineProvider>
       <DevTool />
-      {isMainPageHeader ? <MainPageNavbar /> : <Header />}
-      {!isMainPageHeader && <Sidebar />}
+      {isMainPageHeader ? <Navbar /> : !isLoginSignup ? <Header /> : null}
+      {!isMainPageHeader && !isLoginSignup && <Sidebar />}
 
       <Switch>
         {/* <Route exact path="/" component={LandingPage} /> */}
@@ -124,6 +128,8 @@ function App() {
         <Route exact path={path("/downloadTo")} component={DownloadTO} />
         <Route exact path={path("/pdf")} component={PDF} />
         <Route exact path={path("/complete")} component={Complete} />
+        <Route exact path={path("/login")} component={Login} />
+        <Route exact path={path("/signup")} component={Signup} />
         {process.env.NODE_ENV === "production" && (
           <Redirect from="/contract/*" to="/contract" />
         )}
